ec461057c00dc33ad0351f9b14f298c6663c6f3f
Keith Suderman <suderman@anc.org>
2014-02-23 16:35:54 -0500

    Added Features.Sentence class.

--- src/main/java/org/lappsgrid/vocabulary/Features.java
+++ src/main/java/org/lappsgrid/vocabulary/Features.java
@@ -18,11 +18,23 @@ public class Features
 
    public static class Token
    {
+      public static final String ID = "id";
+      public static final String START = "start";
+      public static final String END = "end";
       public static final String PART_OF_SPEECH = "pos";
       public static final String LEMMA = "lemma";
       public static final String WORD = "string";
-      public static final String CATEGORY = "category";
+      public static final String TYPE = "type";
+      public static final String ORTH = "orth";
       public static final String NER = "namedEntity";
 
+   }
+
+   public static class Sentence
+   {
+      public static final String ID = "id";
+      public static final String START = "start";
+      public static final String END = "end";
+      public static final String TYPE = "type";
    }
 }
